0fbb52f34232ff2d908a62c7cfc2da0330e4efa0
JWL4402 <j.lebert4402@gmail.com>
2023-05-19 12:37:27 -0400

    fix ; made it so that you don't skip over the gap if you run into the wall

--- Dwarven Rush/Assets/Scripts/GapScript.cs
+++ Dwarven Rush/Assets/Scripts/GapScript.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class Gap : MonoBehaviour
+public class GapScript : MonoBehaviour
 {
     private GameObject floor;
     private GameObject dwarf;
@@ -10,6 +11,7 @@ public class Gap : MonoBehaviour
     private BoxCollider2D floor_collider;
     private BoxCollider2D gap_collider;
     private BoxCollider2D dwarf_collider;
+    private BoxCollider2D[] edge_colliders;
 
     private void OnTriggerStay2D(Collider2D other)
     {
@@ -26,22 +28,41 @@ public class Gap : MonoBehaviour
         if (left_bound < dwarf_left && right_bound > dwarf_right)
         {
             floor_collider.enabled = false;
+            ChangeEdges(true);
+
         }
         else
         {
             floor_collider.enabled = true;
+
+            ChangeEdges(false);
         }
     }
     
     private void OnTriggerExit2D(Collider2D collision)
     {
         floor_collider.enabled = true;
+        ChangeEdges(false);
+    }
+
+    void ChangeEdges(bool new_state)
+    {
+        foreach (BoxCollider2D edge_collider in edge_colliders)
+        {
+            edge_collider.enabled = new_state;
+        }
     }
 
     void Start()
     {
         dwarf = GameObject.FindGameObjectWithTag("Dwarf");
         floor = GameObject.FindGameObjectWithTag("Main Floor");
+
+        List<BoxCollider2D> colliders = new List<BoxCollider2D>();
+        foreach (Transform t in transform) colliders.Add(t.gameObject.GetComponent<BoxCollider2D>());
+        edge_colliders = colliders.ToArray();
+        
+        ChangeEdges(false);
 
         gap_collider = GetComponent<BoxCollider2D>();
         dwarf_collider = dwarf.GetComponent<BoxCollider2D>();
