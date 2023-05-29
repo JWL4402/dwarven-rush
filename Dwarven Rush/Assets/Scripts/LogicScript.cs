9b4bd438a1a702c831508a9b53ae5001e67fc9eb
JWL4402 <j.lebert4402@gmail.com>
2023-05-29 12:16:22 -0400

    added in a game over screen

--- Dwarven Rush/Assets/Scripts/LogicScript.cs
+++ Dwarven Rush/Assets/Scripts/LogicScript.cs
@@ -6,26 +6,32 @@ using UnityEngine.UI;
 
 public class LogicScript : MonoBehaviour
 {
-    private int score;
-    [SerializeField]
-    private TextMeshProUGUI scoreboard;
-
     public enum GameState
     {
         ACTIVE,
         GAME_OVER
     }
 
+    public GameState state = GameState.ACTIVE;
+    private int score;
+    [SerializeField] private TextMeshProUGUI scoreboard;
+    [SerializeField] private GameObject gameover;
+    [SerializeField] private TextMeshProUGUI gameover_score;
+
+
     [ContextMenu("Add Score")]
     public void AddScore(int score_increment = 1)
     {
-        
         score += score_increment;
         scoreboard.text = score.ToString();
     }
 
-    public void ChangeGameState(GameState game_state)
+    [ContextMenu("End Game")]
+    public void EndGame()
     {
+        state = GameState.GAME_OVER;
 
+        gameover_score.text = "Score : " + score;
+        gameover.SetActive(true);
     }
 }
